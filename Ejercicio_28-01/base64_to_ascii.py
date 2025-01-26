8db81ccd9f45e4a4a17d78b912df3b1bc6e0f80d
AMontoyaQP <montoyaw1@gmail.com>
2025-01-26 16:13:36 -0600

    Create base64_to_ascii.py

---
+++ Ejercicio_28-01/base64_to_ascii.py
@@ -0,0 +1,15 @@
+import sys
+import base64_to_bin
+import bin_to_ascii
+
+def base64_to_ascii(base64):
+    # get the binary representation of the base64 code
+    binary = base64_to_bin.base64_to_bin(base64)
+    print(binary)
+    # get the ascii representation of the binary code
+    return bin_to_ascii.bin_to_ascii(binary)
+
+if __name__ == "__main__":
+    arg = sys.argv[1]
+    res = base64_to_ascii(arg)
+    print('The ascii code of the base64 representation "'+ arg + '" is', res)
