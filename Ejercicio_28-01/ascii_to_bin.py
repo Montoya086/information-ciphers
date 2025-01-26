a394ee1bc3a4b62e2d07bad8d06008f1fd472b08
AMontoyaQP <montoyaw1@gmail.com>
2025-01-26 15:37:17 -0600

    fix: ceros to the left

--- Ejercicio_28-01/ascii_to_bin.py
+++ Ejercicio_28-01/ascii_to_bin.py
@@ -1,6 +1,7 @@
 import sys
+import complete_bin
 
-def ascii_to_bin(ascii):
+def ascii_to_bin_char(ascii):
     # get the ascii code of the character
     pos = ord(ascii)
     # buffer to store the binary representation
@@ -12,10 +13,19 @@ def ascii_to_bin(ascii):
         # get the integer division by 2 to continue the process
         pos = pos // 2
 
+    return complete_bin.complete_bin(binary)
+
+def ascii_to_bin(ascii):
+    # buffer to store the binary representation
+    binary = ""
+    # iterate over the characters of the string
+    for char in ascii:
+        # get the binary representation of the character
+        binary += ascii_to_bin_char(char) + " "
     return binary
 
 
 if __name__ == "__main__":
     arg = sys.argv[1]
     res = ascii_to_bin(arg)
-    print('The binary representation of the ASCII code of', arg, 'is', res)
+    print('The binary representation of the ASCII code of "'+ arg + '" is', res)
