d2024a5b1e78ad7385c99f2b8d97c2b1bb0962d9
AMontoyaQP <montoyaw1@gmail.com>
2025-02-01 17:34:50 -0600

    Create caesar.py

---
+++ Lab1-A/P1/caesar.py
@@ -0,0 +1,55 @@
+# Cifrado cesar
+import sys
+
+KEY = 3
+
+alphabet = "abcdefghijklmnñopqrstuvwxyz"
+
+def cypher(text, key):
+    # convert text to lowercase
+    text = text.lower()
+    # result string
+    result = ""
+    # iterate over each letter in the text
+    for letter in text:
+        # if the letter is in the alphabet
+        if letter in alphabet:
+            # add the padded letter to the result
+            result += alphabet[(alphabet.index(letter) + key) % len(alphabet)]
+        else:
+            # if the letter is not in the alphabet, add it to the result
+            result += letter
+    return result
+
+def decypher(text, key):
+    # convert text to lowercase
+    text = text.lower()
+    # result string
+    result = ""
+    # iterate over each letter in the text
+    for letter in text:
+        # if the letter is in the alphabet
+        if letter in alphabet:
+            # add the padded letter to the result
+            result += alphabet[(alphabet.index(letter) - key) % len(alphabet)]
+        else:
+            # if the letter is not in the alphabet, add it to the result
+            result += letter
+    return result
+
+if __name__ == "__main__":
+    print(len(alphabet))
+    if len(sys.argv) != 2:
+        print("Usage: python caesar.py <-d|-c> (d: decypher, c: cypher)")
+        sys.exit(1)
+    
+    mode = sys.argv[1]
+
+    if mode == "-c":
+        text = input("Enter the text to cypher: ")
+        print(cypher(text, KEY))
+    elif mode == "-d":
+        text = input("Enter the text to decypher: ")
+        print(decypher(text, KEY))
+
+
