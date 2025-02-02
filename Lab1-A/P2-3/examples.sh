381a84be3bc52b31a7c9e779a56e82a7bb335754
AMontoyaQP <montoyaw1@gmail.com>
2025-02-01 20:16:36 -0600

    freq analysis

---
+++ Lab1-A/P2-3/examples.sh
@@ -0,0 +1,3 @@
+python3 freq.py -p << EOF
+jzlvpwxdk
+EOF
--- Lab1-A/P2-3/freq.py
+++ Lab1-A/P2-3/freq.py
@@ -1,8 +1,44 @@
 import sys
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import tabulate
 # get the absolute path of this file
 absolutepath = sys.path[0]
 sys.path.append(absolutepath + "/../P1")
 from clean import clean_text
+
+# theorical frequency of the spanish language
+theorical_freq = {
+    "a": 0.1253,
+    "b": 0.0142,
+    "c": 0.0468,
+    "d": 0.0586,
+    "e": 0.1368,
+    "f": 0.0069,
+    "g": 0.0101,
+    "h": 0.0070,
+    "i": 0.0625,
+    "j": 0.0044,
+    "k": 0.0002,
+    "l": 0.0497,
+    "m": 0.0315,
+    "n": 0.0671,
+    "ñ": 0.0031,
+    "o": 0.0868,
+    "p": 0.0251,
+    "q": 0.0088,
+    "r": 0.0687,
+    "s": 0.0798,
+    "t": 0.0463,
+    "u": 0.0291,
+    "v": 0.0111,
+    "w": 0.0008,
+    "x": 0.0022,
+    "y": 0.0088,
+    "z": 0.0047
+}
+
 alphabet = "abcdefghijklmnñopqrstuvwxyz"
 
 def calc_freq(text, alphabet):
@@ -37,12 +73,51 @@ def generate_table(text, alphabet):
     prob = calc_prob(freq, total)
     return freq, prob
 
+
+def plot_freq(freq, title):
+    # compare the theorical frequency with the frequency of the text
+    df = pd.DataFrame({
+        "Letter": list(freq.keys()),
+        "Real": list(freq.values()),
+        "Theoretical": [theorical_freq[letter] for letter in freq.keys()]
+    })
+    # restructure the data for side-by-side bars
+    df_melted = pd.melt(df, 
+                       id_vars=['Letter'], 
+                       value_vars=['Real', 'Theoretical'],
+                       var_name='Type',
+                       value_name='Frequency')
+    
+    # create the plot
+    sns.set_theme(style="whitegrid")
+    plt.figure(figsize=(10, 6))
+    sns.barplot(data=df_melted, 
+                x="Letter", 
+                y="Frequency", 
+                hue="Type",
+                palette=["skyblue", "red"])
+    
+    plt.title(title)
+    plt.legend()
+    plt.show()  
+
 if __name__ == "__main__":
     text = input("Enter text: ")
     text = clean_text(text, alphabet)
 
+    should_plot = False
+
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "-p":
+            should_plot = True
+        else:
+            print("Invalid argument")
+            sys.exit(1)
+
     freq, prob = generate_table(text, alphabet)
     print("Frequency table:")
-    print(freq)
+    print(tabulate.tabulate(freq.items(), headers=["Letter", "Frequency"]))
     print("Probability table:")
-    print(prob)
+    print(tabulate.tabulate(prob.items(), headers=["Letter", "Probability"]))
+    if should_plot:
+        plot_freq(prob, "Theorical vs Text frequency of the spanish language")
