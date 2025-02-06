ac45477a11fe48bb2a954e0d746d7576c65ef2a1
AMontoyaQP <montoyaw1@gmail.com>
2025-02-05 23:43:41 -0600

    Create ascii_to_base64.py

---
+++ Lab2-A/ascii_to_base64.py
@@ -0,0 +1,17 @@
+from utils import ascii_to_bin, bin_to_base64
+
+def ascii_to_base64(ascii):
+    return bin_to_base64.bin_to_base64(ascii_to_bin.ascii_to_bin(ascii))
+
+if __name__ == "__main__":
+    # Example 1
+    arg = "Hello!!"
+    res = ascii_to_base64(arg)
+
+    print('The base64 representation of the ASCII code of "'+ arg + '" is', res)
+
+    # Example 2
+    arg = "Hello world"
+    res = ascii_to_base64(arg)
+
+    print('The base64 representation of the ASCII code of "'+ arg + '" is', res)
