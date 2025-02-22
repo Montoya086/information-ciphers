ed9ed2d2a0bb89e94d8544a33a604774e905e947
AMontoyaQP <montoyaw1@gmail.com>
2025-02-22 15:45:06 -0600

    keygen

---
+++ Ejercicio_25-02/keygen.py
@@ -0,0 +1,31 @@
+import os
+import random
+import string
+
+def generate_key(length=16, seed=None):
+    if seed is None:
+        seed = os.urandom(length)
+    # set the seed for the random number generator
+    random.seed(seed)
+    # generate a random number key using only the digits
+    key = ''.join(random.choices(string.digits, k=length))
+    return key
+
+if __name__ == "__main__":
+    print("-"*50)
+    print("Example 1:")
+    length = 16
+    seed = "1234567890"
+    print(f"length: {length}")
+    print(f"seed: {seed}")
+    print(f"key: {generate_key(length, seed)}")
+    print("-"*50+"\n")
+
+    print("-"*50)
+    print("Example 2:")
+    length = 32
+    seed = "asdf1234"
+    print(f"length: {length}")
+    print(f"seed: {seed}")
+    print(f"key: {generate_key(length, seed)}")
+    print("-"*50+"\n")
