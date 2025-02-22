3dc7f341a2d01ff21c6e31da42276e3854154d62
AMontoyaQP <montoyaw1@gmail.com>
2025-02-22 17:41:40 -0600

    typo

--- Ejercicio_25-02/main.py
+++ Ejercicio_25-02/main.py
@@ -5,13 +5,13 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     if len(args) == 0:
         plain_text = "Hello world!!!"
-        nonce = "1234567890"
+        keystream = "1234567890"
     else:
         plain_text = args[0]
-        nonce = args[1]
-    cyphered = cypher(plain_text, nonce, False)
+        keystream = args[1]
+    cyphered = cypher(plain_text, keystream, False)
     print(f"cyphered text: {cyphered}")
-    decyphered = decypher(cyphered, nonce, False)
+    decyphered = decypher(cyphered, keystream, False)
     print(f"decyphered text: {decyphered}")
     
     
--- Ejercicio_25-02/main.test.py
+++ Ejercicio_25-02/main.test.py
@@ -4,9 +4,9 @@ from cypher import cypher, decypher
 class TestCypher(unittest.TestCase):
     def test_cypher(self):
         plain_text = "Hello world!!!"
-        nonce = "1234567890"
-        cyphered = cypher(plain_text, nonce, False)
-        decyphered = decypher(cyphered, nonce, False)
+        keystream = "1234567890"
+        cyphered = cypher(plain_text, keystream, False)
+        decyphered = decypher(cyphered, keystream, False)
         self.assertEqual(decyphered, plain_text)
 
 if __name__ == "__main__":
